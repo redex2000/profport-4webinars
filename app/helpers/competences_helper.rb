f6777956e3c174834813fb6decc43c981c911556
Artur Galyamov <redex2000@gmail.com>
2016-09-14 19:04:06 +0500

    Реализовано редактирование и удаление компетенции, выведены сообщения closes #2641 @20m

--- app/helpers/competences_helper.rb
+++ app/helpers/competences_helper.rb
@@ -1,2 +1,20 @@
 module CompetencesHelper
+  def rails_to_bootstrap_type(type)
+    case type
+      when 'error'
+        'alert-danger'
+      when 'notice'
+        'alert-info'
+      else
+        type
+    end
+
+  end
+
+  def show_message(type, message)
+    content_tag( :div,
+                link_to("&times;".html_safe, '#', class: 'close', data: { dismiss: 'alert' }, aria: { label: 'close' }) +
+                message,
+                class: 'alert fade in '+rails_to_bootstrap_type(type) )
+  end
 end
