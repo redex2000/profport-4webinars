24cf7aa65add8e2c02841cf32f6b90de1955f06e
Artur Galyamov <redex2000@gmail.com>
2016-09-16 21:46:04 +0500

    Реализованы простейшие модульные тесты для компетенции refs #2646 @15m

--- app/models/competence.rb
+++ app/models/competence.rb
@@ -1,2 +1,4 @@
 class Competence < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 255 }
 end
---
+++ spec/models/competence_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Competence, type: :model do
+  describe 'save' do
+    before(:each) { @competence = Competence.new(name: 'Программирование на Rails5') }
+
+    it 'valid with all fields' do
+      expect(@competence.save).to be_truthy
+    end
+
+    it 'not valid without name' do
+      @competence.name = nil
+      expect(@competence.save).not_to be_truthy
+    end
+
+    it 'valid with long name' do
+      @competence.name = 'a' * 255
+      expect(@competence.save).to be_truthy
+    end
+
+    it 'not valid with too long name' do
+      @competence.name = 'a' * 256
+      expect(@competence.save).not_to be_truthy
+    end
+  end
+end
